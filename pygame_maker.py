d194c5d0e17d6b902e2d0559428adc48367cd7f3
Ron Lockwood-Childs <rchilds@vctlabs.com>
2015-03-22 13:57:53 -0700

    Add pygame_maker.py main app

---
+++ pygame_maker.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python -Wall
+
+# implement a "game maker" app using pygame
+
+import pg_template
+
+class PyGameMaker:
+    RESOURCE_TYPES=[
+        "sprites",
+        "sounds",
+        "backgrounds",
+        "paths",
+        "scripts",
+        "shaders",
+        "fonts",
+        "timelines",
+        "objects",
+        "rooms",
+        "includes",
+        "extensions",
+        "constants",
+        "game_info",
+        "game_settings",
+        "extension_pkgs"
+    ]
+
