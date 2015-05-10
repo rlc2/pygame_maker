a1cb660e709c7b7ffe7b0e94d08dc46e6a0b3f6a
Ron Lockwood-Childs <rchilds@vctlabs.com>
2015-05-09 23:27:04 -0700

    Implement event engine

--- pygame_maker_event_engine.py
+++ pygame_maker_event_engine.py
@@ -19,10 +19,11 @@ class PyGameMakerEventEngine(object):
             register_event_handler():
             Add a handler method reference to the named event
         """
-        if not event_name in self.event_handlers:
+        if not event_name in self.event_handlers.keys():
             self.event_handlers[event_name] = [event_handler]
         else:
             self.event_handlers[event_name].append(event_handler)
+        #print("handlers: {}".format(self.event_handlers))
 
     def queue_event(self, event, **event_kwargs):
         """
@@ -31,10 +32,11 @@ class PyGameMakerEventEngine(object):
             to the event queue.
         """
         ename = event.event_name
-        if not ename in self.event_queues:
-            self.event_queues[ename] = [(event, **event_kwargs)]
+        if not ename in self.event_queues.keys():
+            self.event_queues[ename] = [(event, event_kwargs)]
         else:
-            self.event_queues[ename].append( (event, **event_kwargs) )
+            self.event_queues[ename].append( (event, event_kwargs) )
+        #print("queues: {}".format(self.event_queues))
 
     def transmit_event(self, event_name):
         """
@@ -42,18 +44,63 @@ class PyGameMakerEventEngine(object):
             handlers is found, forward the named event along with a possible
             keyword-argument hash.
         """
-        for evname in self.event_queues:
-            if evname in self.event_handlers:
-                for queued in self.event_queues[evname]:
-                    for handler in self.event_handlers[evname]:
-                        handler[0](queued[1])
+        #print("check for {} handlers..".format(event_name))
+        if event_name in self.event_handlers.keys():
+            #print("found. check for event queues..")
+            for queued in self.event_queues[event_name]:
+                #print("handle queue item {}".format(queued))
+                for handler in self.event_handlers[event_name]:
+                    #print("call handler!")
+                    handler(event_name, queued[1])
 
     def transmit_event_type(self, event_type):
         """
             transmit_event_type():
-            Handle all events of the given event type.
+            Call handlers for all events of the given event type.
         """
         for event_name in event_type.HANDLED_EVENTS:
             self.transmit_event(event_name)
 
+if __name__ == "__main__":
+    import unittest
+
+    class TestPyGameMakerEventEngine(unittest.TestCase):
+
+        def setUp(self):
+            self.event_engine = PyGameMakerEventEngine()
+            self.called_events = []
+ 
+        def event_handler(self, ev_name, ev_tag, **kwargs):
+            print("{} received event {}. kwargs: {}".format(ev_tag, ev_name, kwargs))
+            self.called_events.append("{} {}".format(ev_name, ev_tag))
+
+        def test_005event_handling(self):
+            self.called_events = []
+            self.event_engine.register_event_handler('left_pressed',
+                lambda name, kws: self.event_handler(name, 'hdlr1', **kws))
+            self.event_engine.register_event_handler('left_pressed',
+                lambda name, kws: self.event_handler(name, 'hdlr2', **kws))
+            self.event_engine.register_event_handler('normal_step',
+                lambda name, kws: self.event_handler(name, 'hdlr1', **kws))
+            self.event_engine.queue_event(pygm_event.PyGameMakerStepEvent('begin_step'))
+            self.event_engine.queue_event(pygm_event.PyGameMakerMouseEvent('left_pressed'), x=20, y=42)
+            self.event_engine.queue_event(pygm_event.PyGameMakerMouseEvent('left_pressed'), x=40, y=62)
+            self.event_engine.queue_event(pygm_event.PyGameMakerMouseEvent('left_pressed'), x=0, y=0)
+            self.event_engine.queue_event(pygm_event.PyGameMakerStepEvent('normal_step'))
+            self.event_engine.transmit_event('begin_step')
+            self.event_engine.transmit_event('left_pressed')
+            self.event_engine.transmit_event('normal_step')
+            print("event handler calls:\n{}".format(self.called_events))
+            expected_calls = [
+                'left_pressed hdlr1',
+                'left_pressed hdlr2',
+                'left_pressed hdlr1',
+                'left_pressed hdlr2',
+                'left_pressed hdlr1',
+                'left_pressed hdlr2',
+                'normal_step hdlr1'
+            ]
+            self.assertEqual(self.called_events, expected_calls)
+
+    unittest.main()
 
