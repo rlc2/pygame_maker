fe548cb23cb15d4d0806e96ac038ccc4ed87f781
Ron Lockwood-Childs <rchilds@vctlabs.com>
2015-05-25 14:48:36 -0700

    Make mouse, kb event names easier to match
    Mouse event names now start with "mouse_" prefix.
    Keyboard event names now start with "kb_" prefix.
    This should make it easier to provide a single event handler for all
     keyboard and/or mouse events.

--- pygame_maker_event.py
+++ pygame_maker_event.py
@@ -107,23 +107,23 @@ class PyGameMakerStepEvent(PyGameMakerEvent):
 
 class PyGameMakerMouseEvent(PyGameMakerEvent):
     MOUSE_EVENTS=[
-        "button_left",
-        "button_right",
-        "button_middle",
-        "button_6"
-        "button_7"
-        "button_8"
-        "nobutton",
-        "left_pressed",
-        "right_pressed",
-        "middle_pressed",
-        "left_released",
-        "right_released",
-        "middle_released",
+        "mouse_button_left",
+        "mouse_button_right",
+        "mouse_button_middle",
+        "mouse_button_6"
+        "mouse_button_7"
+        "mouse_button_8"
+        "mouse_nobutton",
+        "mouse_left_pressed",
+        "mouse_right_pressed",
+        "mouse_middle_pressed",
+        "mouse_left_released",
+        "mouse_right_released",
+        "mouse_middle_released",
         "mouse_enter",
         "mouse_leave",
-        "mousewheelup",
-        "mousewheeldown"
+        "mouse_wheelup",
+        "mouse_wheeldown"
     ]
     HANDLED_EVENTS=MOUSE_EVENTS
 
@@ -178,71 +178,71 @@ class PyGameMakerDrawEvent(PyGameMakerEvent):
 
 class PyGameMakerKeyEvent(PyGameMakerEvent):
     ARROW_KEYS=[
-        "left",
-        "right",
-        "up",
-        "down",
+        "kb_left",
+        "kb_right",
+        "kb_up",
+        "kb_down",
     ]
     META_KEYS=[
-        "lctrl",
-        "rctrl",
-        "lalt",
-        "ralt",
-        "lshift",
-        "rshift",
-        "lmeta",
-        "rmeta",
-        "lsuper",
-        "rsuper"
+        "kb_lctrl",
+        "kb_rctrl",
+        "kb_lalt",
+        "kb_ralt",
+        "kb_lshift",
+        "kb_rshift",
+        "kb_lmeta",
+        "kb_rmeta",
+        "kb_lsuper",
+        "kb_rsuper"
     ]
     KEYPAD_KEYS=[
-        "np0",
-        "np1",
-        "np2",
-        "np3",
-        "np4",
-        "np5",
-        "np6",
-        "np7",
-        "np8",
-        "np9",
-        "np/",
-        "np*",
-        "np-",
-        "np+",
-        "np.",
-        "np=",
-        "npenter",
+        "kb_np0",
+        "kb_np1",
+        "kb_np2",
+        "kb_np3",
+        "kb_np4",
+        "kb_np5",
+        "kb_np6",
+        "kb_np7",
+        "kb_np8",
+        "kb_np9",
+        "kb_np/",
+        "kb_np*",
+        "kb_np-",
+        "kb_np+",
+        "kb_np.",
+        "kb_np=",
+        "kb_npenter",
     ]
     PUNCTUATION_KEYS=[
-        ",",
-        ".",
-        "/",
-        ";",
-        "'",
-        "[",
-        "]",
-        "-",
-        "=",
-        "`",
-        "\\"
+        "kb_,",
+        "kb_.",
+        "kb_/",
+        "kb_;",
+        "kb_'",
+        "kb_[",
+        "kb_]",
+        "kb_-",
+        "kb_=",
+        "kb_`",
+        "kb_\\"
     ]
-    DIGIT_KEYS=[str(k) for k in range(0,10)]
-    LETTER_KEYS=[chr(l) for l in range(65, 65+26)]
-    FUNCTION_KEYS=["F{}".format(n) for n in range(1,13)]
+    DIGIT_KEYS=["kb_{}".format(str(k)) for k in range(0,10)]
+    LETTER_KEYS=["kb_{}".format(chr(l)) for l in range(65, 65+26)]
+    FUNCTION_KEYS=["kb_F{}".format(n) for n in range(1,13)]
     OTHER_KEYS=[
-        "no_key",
-        "any_key",
-        "space",
-        "enter",
-        "backspace",
-        "escape",
-        "home",
-        "end",
-        "pageup",
-        "pagedown",
-        "delete",
-        "insert"
+        "kb_no_key",
+        "kb_any_key",
+        "kb_space",
+        "kb_enter",
+        "kb_backspace",
+        "kb_escape",
+        "kb_home",
+        "kb_end",
+        "kb_pageup",
+        "kb_pagedown",
+        "kb_delete",
+        "kb_insert"
     ]
     KEYBOARD_EVENT_KEY_CATEGORIES={
         "arrows":   ARROW_KEYS,
@@ -257,106 +257,106 @@ class PyGameMakerKeyEvent(PyGameMakerEvent):
         LETTER_KEYS + PUNCTUATION_KEYS + FUNCTION_KEYS + OTHER_KEYS)
 
     KEY_EVENT_TO_PYGAME_KEY_MAP={
-        ",":        pygame.K_COMMA,
-        ".":        pygame.K_PERIOD,
-        "/":        pygame.K_SLASH,
-        ";":        pygame.K_SEMICOLON,
-        "'":        pygame.K_QUOTE,
-        "[":        pygame.K_LEFTBRACKET,
-        "]":        pygame.K_RIGHTBRACKET,
-        "-":        pygame.K_MINUS,
-        "=":        pygame.K_EQUALS,
-        "`":        pygame.K_BACKQUOTE,
-        "\\":       pygame.K_BACKSLASH,
-        "0":        pygame.K_0,
-        "1":        pygame.K_1,
-        "2":        pygame.K_2,
-        "3":        pygame.K_3,
-        "4":        pygame.K_4,
-        "5":        pygame.K_5,
-        "6":        pygame.K_6,
-        "7":        pygame.K_7,
-        "8":        pygame.K_8,
-        "9":        pygame.K_9,
-        "A":        pygame.K_a,
-        "B":        pygame.K_b,
-        "C":        pygame.K_c,
-        "D":        pygame.K_d,
-        "E":        pygame.K_e,
-        "F":        pygame.K_f,
-        "G":        pygame.K_g,
-        "H":        pygame.K_h,
-        "I":        pygame.K_i,
-        "J":        pygame.K_j,
-        "K":        pygame.K_k,
-        "L":        pygame.K_l,
-        "M":        pygame.K_m,
-        "N":        pygame.K_n,
-        "O":        pygame.K_o,
-        "P":        pygame.K_p,
-        "Q":        pygame.K_q,
-        "R":        pygame.K_r,
-        "S":        pygame.K_s,
-        "T":        pygame.K_t,
-        "U":        pygame.K_u,
-        "V":        pygame.K_v,
-        "W":        pygame.K_w,
-        "X":        pygame.K_x,
-        "Y":        pygame.K_y,
-        "Z":        pygame.K_z,
-        "left":     pygame.K_LEFT,
-        "right":    pygame.K_RIGHT,
-        "up":       pygame.K_UP,
-        "down":     pygame.K_DOWN,
-        "lctrl":    pygame.K_LCTRL,
-        "rctrl":    pygame.K_RCTRL,
-        "lalt":     pygame.K_LALT,
-        "ralt":     pygame.K_RALT,
-        "lshift":   pygame.K_LSHIFT,
-        "rshift":   pygame.K_RSHIFT,
-        "lmeta":    pygame.K_LMETA,
-        "rmeta":    pygame.K_RMETA,
-        "lsuper":   pygame.K_LSUPER,
-        "rsuper":   pygame.K_RSUPER,
-        "space":    pygame.K_SPACE,
-        "enter":    pygame.K_RETURN,
-        "backspace":pygame.K_BACKSPACE,
-        "escape":   pygame.K_ESCAPE,
-        "home":     pygame.K_HOME,
-        "end":      pygame.K_END,
-        "pageup":   pygame.K_PAGEUP,
-        "pagedown": pygame.K_PAGEDOWN,
-        "delete":   pygame.K_DELETE,
-        "insert":   pygame.K_INSERT,
-        "np0":      pygame.K_KP0,
-        "np1":      pygame.K_KP1,
-        "np2":      pygame.K_KP2,
-        "np3":      pygame.K_KP3,
-        "np4":      pygame.K_KP4,
-        "np5":      pygame.K_KP5,
-        "np6":      pygame.K_KP6,
-        "np7":      pygame.K_KP7,
-        "np8":      pygame.K_KP8,
-        "np9":      pygame.K_KP9,
-        "np/":      pygame.K_KP_DIVIDE,
-        "np*":      pygame.K_KP_MULTIPLY,
-        "np-":      pygame.K_KP_MINUS,
-        "np+":      pygame.K_KP_PLUS,
-        "np.":      pygame.K_KP_PERIOD,
-        "np=":      pygame.K_KP_EQUALS,
-        "npenter":  pygame.K_KP_ENTER,
-        "F1":       pygame.K_F1,
-        "F2":       pygame.K_F2,
-        "F3":       pygame.K_F3,
-        "F4":       pygame.K_F4,
-        "F5":       pygame.K_F5,
-        "F6":       pygame.K_F6,
-        "F7":       pygame.K_F7,
-        "F8":       pygame.K_F8,
-        "F9":       pygame.K_F9,
-        "F10":      pygame.K_F10,
-        "F11":      pygame.K_F11,
-        "F12":      pygame.K_F12
+        "kb_,":        pygame.K_COMMA,
+        "kb_.":        pygame.K_PERIOD,
+        "kb_/":        pygame.K_SLASH,
+        "kb_;":        pygame.K_SEMICOLON,
+        "kb_'":        pygame.K_QUOTE,
+        "kb_[":        pygame.K_LEFTBRACKET,
+        "kb_]":        pygame.K_RIGHTBRACKET,
+        "kb_-":        pygame.K_MINUS,
+        "kb_=":        pygame.K_EQUALS,
+        "kb_`":        pygame.K_BACKQUOTE,
+        "kb_\\":       pygame.K_BACKSLASH,
+        "kb_0":        pygame.K_0,
+        "kb_1":        pygame.K_1,
+        "kb_2":        pygame.K_2,
+        "kb_3":        pygame.K_3,
+        "kb_4":        pygame.K_4,
+        "kb_5":        pygame.K_5,
+        "kb_6":        pygame.K_6,
+        "kb_7":        pygame.K_7,
+        "kb_8":        pygame.K_8,
+        "kb_9":        pygame.K_9,
+        "kb_A":        pygame.K_a,
+        "kb_B":        pygame.K_b,
+        "kb_C":        pygame.K_c,
+        "kb_D":        pygame.K_d,
+        "kb_E":        pygame.K_e,
+        "kb_F":        pygame.K_f,
+        "kb_G":        pygame.K_g,
+        "kb_H":        pygame.K_h,
+        "kb_I":        pygame.K_i,
+        "kb_J":        pygame.K_j,
+        "kb_K":        pygame.K_k,
+        "kb_L":        pygame.K_l,
+        "kb_M":        pygame.K_m,
+        "kb_N":        pygame.K_n,
+        "kb_O":        pygame.K_o,
+        "kb_P":        pygame.K_p,
+        "kb_Q":        pygame.K_q,
+        "kb_R":        pygame.K_r,
+        "kb_S":        pygame.K_s,
+        "kb_T":        pygame.K_t,
+        "kb_U":        pygame.K_u,
+        "kb_V":        pygame.K_v,
+        "kb_W":        pygame.K_w,
+        "kb_X":        pygame.K_x,
+        "kb_Y":        pygame.K_y,
+        "kb_Z":        pygame.K_z,
+        "kb_left":     pygame.K_LEFT,
+        "kb_right":    pygame.K_RIGHT,
+        "kb_up":       pygame.K_UP,
+        "kb_down":     pygame.K_DOWN,
+        "kb_lctrl":    pygame.K_LCTRL,
+        "kb_rctrl":    pygame.K_RCTRL,
+        "kb_lalt":     pygame.K_LALT,
+        "kb_ralt":     pygame.K_RALT,
+        "kb_lshift":   pygame.K_LSHIFT,
+        "kb_rshift":   pygame.K_RSHIFT,
+        "kb_lmeta":    pygame.K_LMETA,
+        "kb_rmeta":    pygame.K_RMETA,
+        "kb_lsuper":   pygame.K_LSUPER,
+        "kb_rsuper":   pygame.K_RSUPER,
+        "kb_space":    pygame.K_SPACE,
+        "kb_enter":    pygame.K_RETURN,
+        "kb_backspace":pygame.K_BACKSPACE,
+        "kb_escape":   pygame.K_ESCAPE,
+        "kb_home":     pygame.K_HOME,
+        "kb_end":      pygame.K_END,
+        "kb_pageup":   pygame.K_PAGEUP,
+        "kb_pagedown": pygame.K_PAGEDOWN,
+        "kb_delete":   pygame.K_DELETE,
+        "kb_insert":   pygame.K_INSERT,
+        "kb_np0":      pygame.K_KP0,
+        "kb_np1":      pygame.K_KP1,
+        "kb_np2":      pygame.K_KP2,
+        "kb_np3":      pygame.K_KP3,
+        "kb_np4":      pygame.K_KP4,
+        "kb_np5":      pygame.K_KP5,
+        "kb_np6":      pygame.K_KP6,
+        "kb_np7":      pygame.K_KP7,
+        "kb_np8":      pygame.K_KP8,
+        "kb_np9":      pygame.K_KP9,
+        "kb_np/":      pygame.K_KP_DIVIDE,
+        "kb_np*":      pygame.K_KP_MULTIPLY,
+        "kb_np-":      pygame.K_KP_MINUS,
+        "kb_np+":      pygame.K_KP_PLUS,
+        "kb_np.":      pygame.K_KP_PERIOD,
+        "kb_np=":      pygame.K_KP_EQUALS,
+        "kb_npenter":  pygame.K_KP_ENTER,
+        "kb_F1":       pygame.K_F1,
+        "kb_F2":       pygame.K_F2,
+        "kb_F3":       pygame.K_F3,
+        "kb_F4":       pygame.K_F4,
+        "kb_F5":       pygame.K_F5,
+        "kb_F6":       pygame.K_F6,
+        "kb_F7":       pygame.K_F7,
+        "kb_F8":       pygame.K_F8,
+        "kb_F9":       pygame.K_F9,
+        "kb_F10":      pygame.K_F10,
+        "kb_F11":      pygame.K_F11,
+        "kb_F12":      pygame.K_F12
     }
     KEYBOARD_UP_SUFFIX = re.compile("(.*)(_keyup)$")
     KEYBOARD_DOWN_SUFFIX = re.compile("(.*)(_keydn)$")
@@ -476,15 +476,15 @@ if __name__ == "__main__":
             self.assertEqual(good_event1.event_name, "create")
 
         def test_010valid_key_events(self):
-            good_event2 = PyGameMakerKeyEvent("F1_keyup")
+            good_event2 = PyGameMakerKeyEvent("kb_F1_keyup")
             print(good_event2)
-            self.assertEqual(good_event2.event_name, "F1")
+            self.assertEqual(good_event2.event_name, "kb_F1")
             self.assertEqual(good_event2.key_event_type, "up")
-            good_event3 = PyGameMakerKeyEvent("npenter_keydn")
+            good_event3 = PyGameMakerKeyEvent("kb_npenter_keydn")
             print(good_event3)
-            self.assertEqual(good_event3.event_name, "npenter")
+            self.assertEqual(good_event3.event_name, "kb_npenter")
             self.assertEqual(good_event3.key_event_type, "down")
-            good_event4 = PyGameMakerEvent.get_event_instance_by_event_name("/_keydn")
+            good_event4 = PyGameMakerEvent.get_event_instance_by_event_name("kb_/_keydn")
             print(good_event4)
             self.assertIs(good_event4.__class__, PyGameMakerKeyEvent)
 
@@ -495,9 +495,9 @@ if __name__ == "__main__":
             self.assertEqual(good_event5.collision_object_name, "obj1")
 
         def test_015valid_mouse_events(self):
-            good_event6 = PyGameMakerMouseEvent("button_middle")
+            good_event6 = PyGameMakerMouseEvent("mouse_button_middle")
             print(good_event6)
-            self.assertEqual(good_event6.event_name, "button_middle")
+            self.assertEqual(good_event6.event_name, "mouse_button_middle")
 
         def test_020valid_alarm_events(self):
             good_event7 = PyGameMakerAlarmEvent("alarm0")
@@ -520,7 +520,7 @@ if __name__ == "__main__":
             self.assertEqual(good_event10.event_name, "gui")
 
         def test_040event_parameters(self):
-            good_event11 = PyGameMakerMouseEvent("button_left",
+            good_event11 = PyGameMakerMouseEvent("mouse_button_left",
                 {"mouse.xy": (43,120)})
             print(good_event11)
             self.assertEqual(good_event11["mouse.xy"], (43,120))
