6b449e42aa40dd4fef175a5f643554510c131f7e
Ron Lockwood-Childs <rchilds@vctlabs.com>
2015-10-22 23:32:42 -0700

    Event engine subclasses PyGameMakerLoggingObject
    Debug messages now display function calls and parameters.
    Unit test sets up log formatting.

--- pygame_maker_event_engine.py
+++ pygame_maker_event_engine.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python -W all
 
+# Author: Ron Lockwood-Childs
+
+# Licensed under LGPL v2.1 (see file COPYING for details)
+
+# define the class that allows other classes to subscribe to and receive events
+
 import re
+import pygame_maker_logging_object as pgm_logging
 import pygame_maker_event as pygm_event
 
-class PyGameMakerEventEngine(object):
+class PyGameMakerEventEngine(pgm_logging.PyGameMakerLoggingObject):
     """
         PyGameMakerEventEngine class:
         The source and target of game-generated events. Events are queued
@@ -11,6 +18,7 @@ class PyGameMakerEventEngine(object):
         accepting particular events.
     """
     def __init__(self):
+        super(PyGameMakerEventEngine, self).__init__(type(self).__name__)
         self.event_handlers = {}
         self.event_queues = {}
 
@@ -19,9 +27,13 @@ class PyGameMakerEventEngine(object):
             register_event_handler():
             Add a handler method reference to the named event
         """
+        self.debug("register_event_handler({}, <hdlr>):".format(event_name))
         if not event_name in self.event_handlers.keys():
+            self.info("  add event handler #1 for {}".format(event_name))
             self.event_handlers[event_name] = [event_handler]
         else:
+            idx = len(self.event_handlers[event_name]) + 1
+            self.info("  add event handler #{} for {}".format(idx, event_name))
             self.event_handlers[event_name].append(event_handler)
         #print("handlers: {}".format(self.event_handlers))
 
@@ -30,10 +42,13 @@ class PyGameMakerEventEngine(object):
             unregister_event_handler():
             Remove a handler method reference from the named event
         """
+        self.debug("unregister_event_handler({}, <hdlr>):".format(event_name))
         if event_name in self.event_handlers.keys():
             if event_handler in self.event_handlers[event_name]:
+                self.info("  remove event handler for {}".format(event_name))
                 self.event_handlers[event_name].remove(event_handler)
                 if len(self.event_handlers[event_name]) == 0:
+                    self.info("  delete last event handler for {}".format(event_name))
                     del(self.event_handlers[event_name])
 
     def queue_event(self, event):
@@ -42,10 +57,14 @@ class PyGameMakerEventEngine(object):
             Add the given event along with a possible keyword-argument hash
             to the event queue.
         """
+        self.debug("queue_event({}):".format(event))
         ename = event.name
         if not ename in self.event_queues.keys():
+            self.debug("  queue event #1 named {}".format(ename))
             self.event_queues[ename] = [event]
         else:
+            idx = len(self.event_queues[ename]) + 1
+            self.debug("  queue event #{} named {}".format(idx, ename))
             self.event_queues[ename].append(event)
         #print("queues: {}".format(self.event_queues))
 
@@ -56,14 +75,19 @@ class PyGameMakerEventEngine(object):
             keyword-argument hash.
         """
         #print("check for {} handlers..".format(event_name))
+        self.debug("transmit_event({}):".format(event_name))
         if event_name in self.event_handlers.keys():
             #print("found. check for event queues..")
+            queue_len = len(self.event_queues[event_name])
+            if queue_len > 0:
+                self.debug("  found {} queued {} events".format(queue_len, event_name))
             for queued in self.event_queues[event_name]:
                 #print("handle queue item {}".format(queued))
-                for handler in self.event_handlers[event_name]:
-                    #print("call handler!")
+                for idx, handler in enumerate(self.event_handlers[event_name]):
+                    self.debug("    call handler #{}".format(idx+1))
                     handler(queued)
             # clear the queue
+            self.debug("  delete queued {} events".format(event_name))
             del(self.event_queues[event_name])
 
     def transmit_event_type(self, event_type):
@@ -71,11 +95,20 @@ class PyGameMakerEventEngine(object):
             transmit_event_type():
             Call handlers for all events of the given event type.
         """
+        self.debug("transmit_event_type({})".format(event_type))
         for event_name in event_type.HANDLED_EVENTS:
             self.transmit_event(event_name)
 
 if __name__ == "__main__":
     import unittest
+    import logging
+
+    eelogger = logging.getLogger("PyGameMakerEventEngine")
+    eehandler = logging.StreamHandler()
+    eeformatter = logging.Formatter("%(levelname)s: %(message)s")
+    eehandler.setFormatter(eeformatter)
+    eelogger.addHandler(eehandler)
+    eelogger.setLevel(logging.DEBUG)
 
     class TestPyGameMakerEventEngine(unittest.TestCase):
 
