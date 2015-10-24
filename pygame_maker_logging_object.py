ad663f340690c2e8c25b89dbd8d6c285cd94cdcd
Ron Lockwood-Childs <rchilds@vctlabs.com>
2015-10-23 23:46:26 -0700

    New PyGameMakerLoggingException; show names in log
    Exceptions subclassed from this one will accept an optional parameter,
     that if passed in will be used as a function call that will be passed
     the exception message as the only parameter.
    Sub-classes of PyGameMakerLoggingObject that have a 'name' attribute
     will now display that name at the end of each log message.

--- pygame_maker_logging_object.py
+++ pygame_maker_logging_object.py
@@ -14,6 +14,12 @@ class Indented(object):
     def __exit__(self, type, value, traceback):
         self.logger.log_indent -= self.logger.indent_size
 
+class PyGameMakerLoggingException(Exception):
+    def __init__(self, msg, logger=None):
+        if logger:
+            logger(msg)
+        self.msg = msg
+
 class PyGameMakerLoggingObject(object):
 
     def __init__(self, logger_name=""):
@@ -29,18 +35,29 @@ class PyGameMakerLoggingObject(object):
         if self.log_indent >= self.indent_size:
             self.log_indent -= self.indent_size
 
+    def get_name_field(self):
+        name_field = ""
+        if hasattr(self, 'name'):
+            name_field = " [{}]".format(self.name)
+        return(name_field)
+
     def debug(self, message):
-        self.logger.debug("{}{}".format(" "*self.log_indent, message))
+        self.logger.debug("{}{}{}".format(" "*self.log_indent, message,
+            self.get_name_field()))
 
     def info(self, message):
-        self.logger.info("{}{}".format(" "*self.log_indent, message))
+        self.logger.info("{}{}{}".format(" "*self.log_indent, message,
+            self.get_name_field()))
 
     def warn(self, message):
-        self.logger.warn("{}{}".format(" "*self.log_indent, message))
+        self.logger.warn("{}{}{}".format(" "*self.log_indent, message,
+            self.get_name_field()))
 
     def error(self, message):
-        self.logger.error("{}{}".format(" "*self.log_indent, message))
+        self.logger.error("{}{}{}".format(" "*self.log_indent, message,
+            self.get_name_field()))
 
     def critical(self, message):
-        self.logger.critical("{}{}".format(" "*self.log_indent, message))
+        self.logger.critical("{}{}{}".format(" "*self.log_indent, message,
+            self.get_name_field()))
 
