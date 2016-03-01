6a49e6bbfcceca9cb0cd0f7a2f730b97ce619263
Ron Lockwood-Childs <rchilds@vctlabs.com>
2016-02-29 23:14:52 -0800

    PEP8 fixes and sphinx docstrings for sounds

--- pygame_maker/sounds/sound.py
+++ pygame_maker/sounds/sound.py
@@ -10,42 +10,47 @@ import pygame
 import os.path
 import yaml
 
+
 class SoundException(Exception):
     pass
 
+
 class Sound(object):
     """
-        Implement simple sound effects or background music loaded from
-        sound files.
+    Implement simple sound effects or background music loaded from sound files.
     """
-    SOUND_TYPES=[
+    #: The available sound types
+    SOUND_TYPES = [
         "effect",
         "music",
         "voice"
     ]
-    DEFAULT_SOUND_PREFIX="snd_"
+    DEFAULT_SOUND_PREFIX = "snd_"
 
     @staticmethod
     def load_from_yaml(yaml_stream, unused=None):
         """
-            Create a new sound from a YAML-formatted file
-            sound_yaml_stream: file or stream object
-            Check each key against known Sound parameters, and use
-            only those parameters to initalize a new sound.
-            Returns:
-                o None, if the yaml-formatted sound is invalid
-                o a new sound, if the YAML fields pass basic checks
-            
-            Expected YAML format:
+        Create a new sound from a YAML-formatted file.  Expected YAML format::
+
             - sound_name1:
                 sound_file: <sound_file_path>
                 sound_type: <type>
                 preload: true|false
             - sound_name2:
                 ...
+
+        Check each key against known Sound parameters, and use only those
+        parameters to initalize a new sound.
+
+        :param yaml_stream: A stream containing YAML-formatted text
+        :type yaml_stream: file-like
+        :param unused: This is a placeholder, since other load_from_yaml()
+            resource methods take an additional argument
+        :return: A list of new sound instances, one for each listed in the
+            YAML stream that pass basic checks; if none are found, returns an
+            empty list
+        :rtype: list
         """
-        yaml_info = None
-        sound_name = Sound.DEFAULT_SOUND_PREFIX
         new_sound_list = []
         yaml_info = yaml.load(yaml_stream)
         if yaml_info:
@@ -65,6 +70,24 @@ class Sound(object):
         return new_sound_list
 
     def __init__(self, sound_name=None, **kwargs):
+        """
+        Initialize a new Sound instance.
+
+        :param sound_name: The name this sound will be referenced by in other
+            resources
+        :type sound_name: str
+        :param kwargs: Sound options passed in keyword args.  The following
+            options are available:
+
+            * ``sound_file`` (str): The path to a file containing the audio
+              data
+            * ``sound_type`` (str): The type of sound this is, see
+              :py:attr:`SOUND_TYPES`
+            * ``preload`` (bool): True if the sound should be loaded before
+              the game loop begins
+
+        :return:
+        """
         if sound_name:
             self.name = sound_name
         else:
@@ -85,14 +108,18 @@ class Sound(object):
 
     def setup(self):
         """
-            setup():
-            Preload the sound if preload is set. Must be done after
-             pygame.init().
+        Preload the sound if preload is set.  Must be done after
+        ``pygame.init().``
         """
         if self.sound_file and self.preload:
             self.load_file()
 
     def load_file(self):
+        """
+        Load the audio data from the file, if it exists.
+
+        :raise: SoundException if the file is not found
+        """
         if not os.path.exists(self.sound_file):
             raise SoundException("SoundException: Sound file '{}' not found.".format(self.sound_file))
         if not self.loaded:
@@ -100,6 +127,11 @@ class Sound(object):
             self.loaded = True
 
     def play_sound(self, loop=False):
+        """
+        Play the sound.  Load it first if it wasn't already loaded.
+
+        :param loop: Loop this sound until stopped using :py:meth:`stop_sound`
+        """
         if not self.loaded:
             self.load_file()
         play_loops = 0
@@ -108,26 +140,51 @@ class Sound(object):
         self.channel = self.audio.play(loops=play_loops)
 
     def stop_sound(self):
+        """Stop the sound from playing."""
         self.audio.stop()
 
     def is_sound_playing(self):
+        """
+        Answer whether the sound is currently playing.
+
+        :return: True if the sound is playing
+        :rtype: bool
+        """
         playing_sound = self.channel.get_sound()
-        return (playing_sound == self.audio)
+        return playing_sound == self.audio
 
     def to_yaml(self):
+        """
+        Generate the YAML string describing this Sound instance.
+
+        :return: The YAML-formatted string
+        :rtype: str
+        """
         ystr = "- {}:\n".format(self.name)
         if self.sound_file:
             ystr += "    sound_file: {}\n".format(self.sound_file)
         ystr += "    sound_type: {}\n".format(self.sound_type)
-        ystr += "    preload: {}\n".format(self.preload)
-        return(ystr)
+        ystr += "    preload: {}\n".format(str(self.preload))
+        return ystr
 
     def check_type(self):
+        """
+        Error-check the ``sound_type`` attribute.
+
+        :raise: SoundException if the type string is unrecognized
+        :return: True if the sound type is known
+        """
         if not self.sound_type in self.SOUND_TYPES:
             raise SoundException("Sound: Unknown sound type '{}'".format(self.sound_type))
         return True
 
     def check(self):
+        """
+        Run error-checking on this sound resource.
+
+        :return: The results from the error check, True if OK
+        :rtype: bool
+        """
         return self.check_type()
 
     def __eq__(self, other):
@@ -136,4 +193,3 @@ class Sound(object):
             (self.sound_file == other.sound_file) and
             (self.sound_type == other.sound_type) and
             (self.preload == other.preload))
-
